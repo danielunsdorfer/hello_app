f5a9ebce88e9feb441dcd33f4405493783cd9f70
Daniel Unsdorfer <daniel.unsdorfer@gmail.com>
2016-03-07 20:50:22 +0000

    Changes test to hola mundo and adds support for UTF-8 characters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  #!/bin/env/ruby
+  # encoding: utf-8
   protect_from_forgery with: :exception
   
   def hello
-    render text: "hello, world!"
+    render text: "\u00A1hola, mundo!"
+  end
+  
+  def goodbye
+    render text: "goodbye, world!"
   end
 end
